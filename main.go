f7145a48ba60ca5678655b42f70187566885d67f
orpheas Vou <orpheas@evline.com>
2024-03-17 16:44:52 +0200

    values

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
 	var revenue float64
 	var expences float64
 	var taxRate float64
+
+	fmt.Print("Enter revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Enter expences: ")
+	fmt.Scan(&expences)
+
+	fmt.Print("Enter tax rate: ")
+	fmt.Scan(&taxRate)
+
+	profit := revenue - expences
+	tax := profit * taxRate / 100
+	profitAfterTax := profit - tax
+	profitBeforeTax := profit + tax
+	ratio := profitAfterTax / revenue
+	fmt.Println("Profit after tax: ", profitAfterTax)
+	fmt.Println("Profit before tax: ", profitBeforeTax)
+	fmt.Println("Ratio:  ", ratio)
 
 }
